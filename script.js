0eb8e426d51da56d4e4325ddedcad70c53602e32
bree melechinsky <groundcontroltomajorbree@gmail.com>
2018-04-15 15:59:32 -0500

    add button and list appending functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log('js check')
+console.log('js check');
 
 $(document).ready(onReady);
 
@@ -9,6 +9,32 @@ function onReady()  {
 }
 
 function clickHandler() {
-    console.log('free click check!');
+    console.log('free clicks!'); //checking if button click works
+    addEmployee();
 }
 
+function addEmployee()    {
+    $('#employeeList').append(
+        '<tr>' +
+            '<td>' + $('#firstName').val() + '</td>' +
+            '<td>' + $('#lastName').val() + '</td>' +
+            '<td>' + $('#idNumber').val() + '</td>' +
+            '<td>' + $('#jobTitle').val() + '</td>' +
+            '<td>' + $('#annualSalary').val() + '</td>' +
+        '</tr>'
+    );
+}
+
+
+
+
+
+
+
+//example test run info
+
+/* '<tr>' + '<td>' + 'Jen' + '</td>'
+'<td>' + 'Barber' + '</td>'
+'<td>' + '4521' + '</td>'
+'<td>' + 'Team Lead' + '</td>'
+'<td>' + '80,000' + '</td>' + '</tr>'*/
